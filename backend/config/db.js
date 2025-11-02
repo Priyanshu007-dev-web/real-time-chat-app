44159734cc40affdc029ee52e7a4422fb9ff8e6e
shoryaakash <shoryaakash@gmail.com>
2025-11-02 10:02:30 +0530

    database connection successfully

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,8 @@ import "dotenv/config"
 
 const connectDb = async () => {
     try {
-        console.log("----aa->", process.env.MONGODB_URL)
         await mongoose.connect(process.env.MONGODB_URL)
-        console.log("⚙️ Database is connected")
+        console.log("⚙️  Database is connected")
     } catch (error) {
         console.log("❌ Database Connection error ==>", error)
     }
